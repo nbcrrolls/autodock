368a1d0fcd9c69a6745c780d895f057b5ed74700
Luca Clementi <luca.clementi@gmail.com>
2012-02-08 02:13:05 +0000

    - Fixed Vision macros which were using ws or kryptonite  - Fixed and test the script to modify the hostname

--- src/mgltools/renameOpalService.sh
+++ src/mgltools/renameOpalService.sh
@@ -1,10 +1,34 @@
+# LC 
+# script that can be used to change the server name 
+# pointed by the opal services in the Vision Network
+# 
 
+#the new rocks server offering the server
 SERVERNAME=$1
-INPUTFILE="VinaVSpublic_0.1_net.py"
 SERVICENAME=`echo $SERVERNAME | sed 's/\./_/g'`
+#basepath of the mgltools
+BASEPATH="/opt/mgltools/MGLToolsPckgs"
 
-sed "s/AutodockVina_Screening_kryptonite_nbcr_net/autodockvina_screening_1_1_2_$SERVICENAME/g" $INPUTFILE > CorrectHost_net.py
-sed -i "s/kryptonite\.nbcr\.net/$SERVERNAME/g" CorrectHost_net.py 
+#cadd workflow
+INPUTFILE="$BASEPATH/CADD/workflows/virtualScreening/VinaVSpublic_0.1_net.py"
+sed -i -e "s/AutodockVina_Screening_kryptonite_nbcr_net/autodockvina_screening_1_1_2_$SERVICENAME/g" \
+       -e "s/kryptonite\.nbcr\.net/$SERVERNAME/g" $INPUTFILE 
 
+INPUTFILE="$BASEPATH/CADD/workflows/MDanalysis/GROMOSClustering_0.1_net.py"
+sed -i -e "s/GROMOS_ClusterFiles_kryptonite_nbcr_net/gromos_cluster_4_5_5_$SERVICENAME/g" \
+       -e "s/kryptonite\.nbcr\.net/$SERVERNAME/g" $INPUTFILE
 
+#modifying the adt macro node
+INPUTFILE="$BASEPATH/AutoDockTools/VisionInterface/Adt/Macro/Vina.py"
+sed -i -e "s/AutodockVina_Screening_kryptonite_nbcr_net/autodockvina_screening_1_1_2_$SERVICENAME/g" \
+       -e "s/kryptonite\.nbcr\.net/$SERVERNAME/g" $INPUTFILE 
+
+INPUTFILE="$BASEPATH/AutoDockTools/VisionInterface/Adt/Macro/GromosCluster.py"
+sed -i -e "s/GROMOS_ClusterFiles_kryptonite_nbcr_net/gromos_cluster_4_5_5_$SERVICENAME/g" \
+       -e "s/kryptonite\.nbcr\.net/$SERVERNAME/g" $INPUTFILE
+
+INPUTFILE="$BASEPATH/AutoDockTools/VisionInterface/Adt/Macro/PrepareReceptor.py"
+sed -i -e "s/Pdb2pqrOpalService_ws_nbcr_net/pdb2pqr_1_7_$SERVICENAME/g" \
+       -e "s/PrepareReceptorOpalService_ws_nbcr_net/prepare_receptor_1_5_6_$SERVICENAME/g" \
+       -e "s/ws\.nbcr\.net/$SERVERNAME/g" $INPUTFILE
 
