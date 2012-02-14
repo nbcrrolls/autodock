ae1ca12abc4e7622edac163571b547c2a6359600
Luca Clementi <luca.clementi@gmail.com>
2012-02-14 20:26:20 +0000

    Fixed the list of available databases Moved to the correct path Fixed the number of CPUs

---
+++ src/mgltools/patch-files/CADD/workflows/virtualScreening/__about__.py
@@ -0,0 +1,4 @@
+
+about = "The Virtual Screening workflows version 0.1 provide tools to run \n"
+about = about + "AutodDck-based virtual screening. Available workflows:\n"
+about = about + "  - VinaVSpublic - virtual screening using AutoDock Vina and ligand libraries on the server\n"
---
+++ src/mgltools/patch-files/CADD/workflows/visualization/__about__.py
@@ -0,0 +1,3 @@
+
+about =         "The Visualization workflows verison 0.1 provide tools\n"
+about = about + "for visualization of the simulation results.\n"
