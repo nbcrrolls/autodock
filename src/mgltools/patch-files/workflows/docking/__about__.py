f51ee1d10ecdc22509d7f2d92e1bd16bd34e6e0f
Luca Clementi <luca.clementi@gmail.com>
2012-02-14 20:30:30 +0000

    Fixing wrong path

--- src/mgltools/patch-files/workflows/docking/__about__.py
+++
@@ -1,3 +0,0 @@
-
-about = "The Docking workflows verison 0.1 provide tools to run separate steps "
-about = about + "of virtual scrreening. Available tools:\n"
--- src/mgltools/patch-files/workflows/virtualScreening/__about__.py
+++
@@ -1,4 +0,0 @@
-
-about = "The Virtual Screening workflows version 0.1 provide tools to run \n"
-about = about + "AutodDck-based virtual screening. Available workflows:\n"
-about = about + "  - VinaVSpublic - virtual screening using AutoDock Vina and ligand libraries on the server\n"
