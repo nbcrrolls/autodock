defbb029c738a41915f02da514408234495fd273
Luca Clementi <luca.clementi@gmail.com>
2012-07-26 22:09:02 +0000

    first attempt to install the new racoon (cadd pipeline)

--- src/mgltools/patch-files/CADD/workflows/MDanalysis/__about__.py
+++
@@ -1,4 +0,0 @@
-
-about = "The MD Analysis workflows provide Molecular dynamics tools:\n"
-about = about + "   GROMOS - trajectory clustering using GROMOS\n"
-about = about + "   TrajQR - trajectory clustering using TrajQR\n"
--- src/mgltools/patch-files/CADD/workflows/visualization/__about__.py
+++
@@ -1,3 +0,0 @@
-
-about =         "The Visualization workflows verison 0.1 provide tools\n"
-about = about + "for visualization of the simulation results.\n"
